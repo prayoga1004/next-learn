47c3127b2b4f5d906203427271b3712795fd7015
surya prayoga <115685664+prayoga1004@users.noreply.github.com>
2025-07-18 12:15:33 +0700

    add index page

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello from DevOps Project!</h1>
+      <p>This is a test deployment for Magang Berdampak</p>
+    </div>
+  );
+}
